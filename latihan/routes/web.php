60c2df40654419dca8e61c65cfdf3273b6889819
Reynaldi Chandra <kennanh2710@gmail.com>
2025-05-17 17:06:48 +0700

    commit

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -1,25 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdiController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function() {
-    return view('beranda', ['name' => 'Kennan Hamilton', 'email' => 'kennanh2710@gmail.com', 'address' => 'Kedamaian Permai 2']);
+Route::get('/home', function(){
+    return view('beranda', 
+    [
+            'name' => 'Nur Rachmat',
+            'email' => 'nurrachmat@gmail.com',
+            'alamat' => 'Palembang'
+        ]
+    );
 });
 
-Route::get('/berita/{id}/{judul?}', function ($id, $judul = null) {
+Route::get('/berita/{id}/{judul?}', function($id, $judul = null){
     return view('berita', ['id' => $id, 'judul' => $judul]);
 });
 
-//membuat route ke halaman prodi index melalui controller ProdiController
-Route::get('/prodi/index', [ProdiController::class,'index']);
+//membuat route ke halam prodi index melalui controller ProdiController
+//Route::get('/prodi/index', [ProdiController::class,'index']);
 
-Route::resource('materi', MateriController::class);
 Route::resource('prodi', ProdiController::class);
-Route::resource('fakultas', FakultasController::class);
-Route::resource('mhs', MahasiswaController::class);
-Route::resource('dosen', DosenController::class);
